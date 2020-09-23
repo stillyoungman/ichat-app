0db79331f4248631fffb8388088f81b0357691bb
Constantine Nikolsky <myheavybox@gmail.com>
2020-09-24 04:29:10 +0900

    Add useful extensions

---
+++ iChat/Extensions/UIImageView.swift
@@ -0,0 +1,15 @@
+//
+//  UIImageView.swift
+//  iChat
+//
+//  Created by Constantine Nikolsky on 23.09.2020.
+//  Copyright © 2020 Constantine Nikolsky. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    var hasImage: Bool {
+        return self.image != nil
+    }
+}
