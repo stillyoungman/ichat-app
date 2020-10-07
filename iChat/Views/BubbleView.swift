933cd939141b9d3a314cd248618e8f47af2b837e
Constantine Nikolsky <myheavybox@gmail.com>
2020-10-08 02:04:58 +0900

    Implement new version of MessageCell

---
+++ iChat/Views/BubbleView.swift
@@ -0,0 +1,125 @@
+//
+//  SpeechBubble.swift
+//  iChat
+//
+//  Created by Constantine Nikolsky on 05.10.2020.
+//  Copyright © 2020 Constantine Nikolsky. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BubbleView: UIView {
+    var color: UIColor? = .clear {
+        didSet {
+            shapeLayer.fillColor = color?.cgColor
+        }
+    }
+    let shapeLayer: CAShapeLayer =  CAShapeLayer()
+    let contentLayoutGuide: UILayoutGuide = {
+        let lg = UILayoutGuide()
+        lg.identifier = "Content Layout Guide"
+        return lg
+    }()
+    private var direction: Direction?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layer.addSublayer(shapeLayer)
+        
+        setupContentLayoutGuide()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.tapped(_:)))
+        self.isUserInteractionEnabled = true
+        self.addGestureRecognizer(tap)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    
+    func setupContentLayoutGuide() {
+        addLayoutGuide(contentLayoutGuide)
+        
+        widthAnchor.constraint(equalTo: contentLayoutGuide.widthAnchor, constant: 30).isActive = true
+        heightAnchor.constraint(equalTo: contentLayoutGuide.heightAnchor, constant: 16).isActive = true
+        centerYAnchor.constraint(equalTo: contentLayoutGuide.centerYAnchor).isActive = true
+    }
+    
+    var contentView: UIView? {
+        didSet {
+            guard let cv = contentView else { return /* could be reasonable to remove from superview old value */ }
+            if let oldValue = oldValue { oldValue.removeFromSuperview() }
+            addSubview(cv)
+            cv.translatesAutoresizingMaskIntoConstraints = false
+            contentLayoutGuide.heightAnchor.constraint(equalTo: cv.heightAnchor).isActive = true
+            contentLayoutGuide.widthAnchor.constraint(equalTo: cv.widthAnchor).isActive = true
+            contentLayoutGuide.topAnchor.constraint(equalTo: cv.topAnchor).isActive = true
+        }
+    }
+    
+    let layoutContentOffset: CGFloat = 4
+    var leadingConstraints: [NSLayoutConstraint] {
+        guard let cv = contentView else { return [] }
+        return [
+            contentLayoutGuide.leadingAnchor.constraint(equalTo: cv.leadingAnchor),
+            centerXAnchor.constraint(equalTo: contentLayoutGuide.centerXAnchor, constant: -layoutContentOffset)
+        ]
+    }
+    
+    var trailingConstraints: [NSLayoutConstraint] {
+        guard let cv = contentView else { return [] }
+        return [
+            contentLayoutGuide.trailingAnchor.constraint(equalTo: cv.trailingAnchor),
+            centerXAnchor.constraint(equalTo: contentLayoutGuide.centerXAnchor, constant: layoutContentOffset)
+        ]
+    }
+    
+    var activeConstraints: [NSLayoutConstraint]?
+    func activateConstraints(for direction: Direction){
+        activeConstraints?.deactivate()
+        self.direction = direction
+        
+        switch direction {
+        case .in:
+            activeConstraints = leadingConstraints
+        case .out:
+            activeConstraints = trailingConstraints
+        default: fatalError("Unsupported direction type.")
+        }
+        
+        activeConstraints?.activate()
+    }
+    
+    @objc func tapped(_ sender: UITapGestureRecognizer){
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+    }
+    
+    var previousFrame: CGRect?
+    func redrawBubbleLayer(for frame: CGRect) {
+        guard let currentDirection = direction else { return }
+        if previousFrame == frame { return }
+        
+        shapeLayer.path = drawPath(for: frame, direction: currentDirection).cgPath
+        previousFrame = frame
+    }
+    
+    private func drawPath(for frame: CGRect, direction: Direction) -> UIBezierPath {
+        let path = SpeechBubblePath.draw(frame: frame)
+        if direction == .in {
+            path.apply(CGAffineTransform(scaleX: -1, y: 1))
+            path.apply(CGAffineTransform(translationX: frame.width, y: 0))
+        }
+
+        return path
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        redrawBubbleLayer(for: bounds)
+    }
+}
--- iChat/Views/MessageCell.swift
+++ iChat/Views/MessageCell.swift
@@ -12,226 +12,101 @@ class MessageCell: UITableViewCell {
     private let outputMessageColor = UIColor.init(hex: "#DCF7C5") ?? UIColor.systemGreen
     private let inputMessageColor = UIColor.init(hex: "#DFDFDF") ?? UIColor.systemGray
     
-    lazy var messageBubble: MessageBuble = MessageBuble()
+    private let bubbleMaxWidthMultiplier: CGFloat = 0.7
     
-    var activeContraints = [NSLayoutConstraint]()
-    
-    private func addSubviews(){
-        addSubview(messageBubble)
-        messageBubble.translatesAutoresizingMaskIntoConstraints = false
-        messageBubble.pinEdges(to: self)
-    }
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        addSubviews()
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        addSubviews()
-    }
-    
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        NSLayoutConstraint.deactivate(activeContraints)
-        print("1-\(messageBubble.constraints.count)")
-        messageBubble.reset()
-        print("2-\(messageBubble.constraints.count)")
+    lazy var bubble: BubbleView = {
+        let bubble = BubbleView()
+        bubble.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(bubble)
         
-    }
-}
-
-extension MessageCell: ConfigurableView {
-    typealias ConfigurationModel = MessageModel
-    
-    func configure(with model: MessageModel) {
-        self.messageBubble.configure(model)
+        let marginGuide = contentView.layoutMarginsGuide
         
-        activeContraints =
-            [
-                messageBubble.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 1),
-                messageBubble.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 1),
-                messageBubble.leadingAnchor.constraint(equalTo: leadingAnchor),
-                messageBubble.topAnchor.constraint(equalTo: topAnchor)
-        ]
+        bubble.widthAnchor.constraint(lessThanOrEqualTo: marginGuide.widthAnchor,
+            multiplier: bubbleMaxWidthMultiplier).isActive = true
+        bubble.topAnchor.constraint(equalTo: marginGuide.topAnchor).isActive = true
+        bubble.bottomAnchor.constraint(equalTo: marginGuide.bottomAnchor).isActive = true
+        bubble.color = outputMessageColor
         
-        NSLayoutConstraint.activate(activeContraints)
-    }
-}
-
-//UIEdgeInsets(top: 10, left: 10, bottom: 70, right: 70) - 21 lines
-
-class MessageBuble: UIView {
-    //    private static let bubbleImage: UIImage = #imageLiteral(resourceName: "messageBubble")
-    //        .resizableImage(withCapInsets:
-    //            UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 30),
-    //                        resizingMode: .stretch).withRenderingMode(.alwaysTemplate)
+        return bubble
+    }()
     
-    var model: MessageModel?
-    
-    private static let bubbleImage: UIImage = #imageLiteral(resourceName: "messageBubble")
-        .resizableImage(withCapInsets:
-            UIEdgeInsets(top: 10, left: 10, bottom: 70, right: 70),
-                        resizingMode: .stretch).withRenderingMode(.alwaysTemplate)
-    
-    private lazy var messageBuble: UIImageView = UIImageView(image: MessageBuble.bubbleImage )
-    
-    private lazy var textView: UITextView = {
+    lazy var textView: UITextView = {
         let textView = UITextView()
-        UIView.performWithoutAnimation({ () -> Void in // fixes iOS 8 blinking when cell appears
-            textView.backgroundColor = UIColor.clear
-        })
+        textView.backgroundColor = .none
+        textView.textContainerInset = .zero
+        textView.font = .systemFont(ofSize: 16)
+        textView.textContainer.lineFragmentPadding = 0
         textView.isEditable = false
         textView.isSelectable = false
+        textView.isScrollEnabled = false
         textView.dataDetectorTypes = .all
         textView.scrollsToTop = false
-        textView.isScrollEnabled = false
         textView.bounces = false
         textView.bouncesZoom = false
         textView.showsHorizontalScrollIndicator = false
         textView.showsVerticalScrollIndicator = false
-        textView.isExclusiveTouch = true
-        textView.textContainer.lineFragmentPadding = 0
-        textView.font = UIFont.systemFont(ofSize: fontSize)
         return textView
     }()
     
-    var attributedText: NSAttributedString?
-    
-    private let outputMessageColor = UIColor.init(hex: "#DCF7C5") ?? UIColor.systemGreen
-    private let inputMessageColor = UIColor.init(hex: "#DFDFDF") ?? UIColor.systemGray
-    let outerBubbleVerticalSpace: CGFloat = 20
-    let innerBubbleVerticalSpace: CGFloat = 12
-    let innerBubbleHorizontalSpace: CGFloat = 15
-    let lineHeight: CGFloat = 19
-    let fontSize: CGFloat = 16
-    let widthMultiplier: CGFloat = 0.7
-    let sidePadding: CGFloat = 10
-    
-    var lineSpacing: CGFloat { (lineHeight - fontSize) / 2 }
-    var height: CGFloat { lineHeight * CGFloat(linesCount) }
-    var availableWidth: CGFloat { messageBuble.frame.width - innerBubbleVerticalSpace }
-    
-    var linesCount: Int {
-        if availableWidth <= 0 {
-            return 0
-            
-        }
-        guard let text = attributedText else { return 0 }
-        
-        let height = text.height(containerWidth: availableWidth)
-        if height < lineHeight { return 1 }
-        let hasReminder = height.truncatingRemainder(dividingBy: lineHeight) == 0
-        let lines = Int(height / lineHeight)
-        return hasReminder ? lines + 1 : lines
+    var incomingConstraints: [NSLayoutConstraint] {
+        [ bubble.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor) ]
     }
     
-    var appliedContraints = [NSLayoutConstraint]()
-    
-    var leadingSideConstraints: [NSLayoutConstraint] {
-        [
-            messageBuble.widthAnchor.constraint(equalTo: widthAnchor, multiplier: widthMultiplier),
-            messageBuble.heightAnchor.constraint(equalToConstant: height),
-            messageBuble.centerYAnchor.constraint(equalTo: centerYAnchor),
-            messageBuble.leadingAnchor.constraint(equalTo: leadingAnchor, constant: sidePadding)
-        ]
+    var outgoingConstraints: [NSLayoutConstraint] {
+        [ bubble.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor) ]
     }
     
-    var trailingSideConstraints: [NSLayoutConstraint] {
-        [
-            messageBuble.widthAnchor.constraint(equalTo: widthAnchor, multiplier: widthMultiplier),
-            messageBuble.heightAnchor.constraint(equalToConstant: height),
-            messageBuble.centerYAnchor.constraint(equalTo: centerYAnchor),
-            messageBuble.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -sidePadding)
-        ]
+    var activeContraints = [NSLayoutConstraint]()
+    func activateConstraints(for direction: Direction) {
+        activeContraints.deactivate()
+        activeContraints = direction == .in ? incomingConstraints : outgoingConstraints
+        activeContraints.activate()
     }
     
-    func heightForLines(_ count: Int) -> CGFloat {
-        trace()
-        "\(count) -> \(CGFloat(lineHeight * CGFloat(count) + innerBubbleVerticalSpace))".log()
-        return CGFloat(lineHeight * CGFloat(count) + innerBubbleVerticalSpace)
+    func setColor(for direction: Direction) {
+        bubble.color = direction == .out ? outputMessageColor : inputMessageColor
     }
     
-    func setupMessageText(text: String?){
-        guard let text = text else { return }
-        let attributeString = NSMutableAttributedString(string: text)
-        let style = NSMutableParagraphStyle()
-        style.lineSpacing = lineSpacing
-        
-        let attributes: [NSAttributedString.Key : Any] = [
-            NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize),
-            NSAttributedString.Key.paragraphStyle: style
-        ]
-        
-        attributeString.addAttributes(attributes, range:  NSMakeRange(0, attributeString.length))
-        self.attributedText = attributeString
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
         initialSetup()
     }
     
-    override var intrinsicContentSize: CGSize {
-        return CGSize(width:sidePadding + frame.width * widthMultiplier , height: height)
-    }
-    
-//    override var intrinsicContentSize: CGSize {
-//        print(CGSize(width: sidePadding + frame.width * widthMultiplier,
-//            height: height + innerBubbleVerticalSpace + outerBubbleVerticalSpace))
-//        print(height)
-//        return CGSize(width: sidePadding + frame.width * widthMultiplier,
-//                      height: height + innerBubbleVerticalSpace + outerBubbleVerticalSpace)
-//    }
-    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        initialSetup()
     }
     
-    func reset() {
-        NSLayoutConstraint.deactivate(appliedContraints)
-        appliedContraints = []
-        attributedText = nil
-        textView.text = ""
-        backgroundColor = .none
-        messageBuble.image = nil
-    }
-    
-    func configure(_ model: MessageModel) {
-        self.model = model
-        setupMessageText(text: model.text)
+    private func initialSetup(){
+        selectionStyle = .none
+        bubble.contentView = textView
         
-        
-        if model.isOutput {
-            
-            messageBuble.tintColor = outputMessageColor
-            messageBuble.image = MessageBuble.bubbleImage
-        } else {
-            
-            messageBuble.tintColor = inputMessageColor
-            messageBuble.image = MessageBuble.bubbleImage.withHorizontallyFlippedOrientation()
+        Utils.debug {
+            backgroundColor = .systemPink
+            contentView.backgroundColor = .yellow
         }
-        appliedContraints =  model.isOutput ? trailingSideConstraints : leadingSideConstraints
-        NSLayoutConstraint.activate(appliedContraints)
     }
     
-    func activateConstraints(){
-        guard let model = model else {
-            return
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        bubble.color = .none
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+}
+
+extension MessageCell: IConfigurable {
+    
+    func configure(with model: IMessage){
+        let direction: Direction = model.isOutgoing ? .out : .in
+        activateConstraints(for: direction)
+        bubble.activateConstraints(for: direction)
+        setColor(for: direction)
+        
+        if let textMessage = model as? TextMessage {
+            textView.text = textMessage.text
         }
-        appliedContraints =  model.isOutput ? trailingSideConstraints : leadingSideConstraints
     }
-    
-    func initialSetup(){
-        messageBuble.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(messageBuble)
-        
-        //        NSLayoutConstraint.activate(trailingSideConstraints)
-        
-        
-        //        messageBuble.tintColor = UIColor.init(hex: "#DCF7C5")!
-    }
-    
-    
 }
